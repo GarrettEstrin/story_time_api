6e7d40826da14b1133b7c6a4eb8e10e292622918
Garrett Estrin <garrett.estrin@gmail.com>
2016-09-19 13:18:59 -0700

    fixed form error for user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -10,7 +10,7 @@ class UsersController < ApplicationController
   end
 
   def create
-    @user = User.new({name: params[:user][:name], email: params [:user][:email]})
+    @user = User.new({name: params[:user][:name], email: params[:user][:email]})
     if @user.save
       redirect_to stories_path
     end
