7bba004a8a9768a2ec51b55c9d5bf6c6c10341d2
Garrett Estrin <garrett.estrin@gmail.com>
2016-09-20 13:06:02 -0700

    added comment model

--- db/schema.rb
+++ db/schema.rb
@@ -11,10 +11,20 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160919202120) do
+ActiveRecord::Schema.define(version: 20160920200212) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "comments", force: :cascade do |t|
+    t.string   "responder"
+    t.text     "body"
+    t.integer  "story_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "comments", ["story_id"], name: "index_comments_on_story_id", using: :btree
 
   create_table "stories", force: :cascade do |t|
     t.string   "title"
@@ -34,5 +44,6 @@ ActiveRecord::Schema.define(version: 20160919202120) do
     t.string   "password_digest"
   end
 
+  add_foreign_key "comments", "stories"
   add_foreign_key "stories", "users"
 end
