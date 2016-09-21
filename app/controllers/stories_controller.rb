21510b12f0339f88ecd81de75a0eb130c491356f
Garrett Estrin <garrett.estrin@gmail.com>
2016-09-20 19:40:00 -0700

    added authors to quotes

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,7 +1,7 @@
 class StoriesController < ApplicationController
   def index
     @stories = Story.all
-    @story_starts = ['A screaming comes across the sky.', 'It was a bright cold day in April, and the clocks were striking thirteen.', 'It was a pleasure to burn.', 'The Man in Black fled across the desert, and the Gunslinger followed.', 'The flash projected the outline of the hanged man onto the wall.', 'If you’re going to read this, don’t bother.', 'Happy families are all alike; every unhappy family is unhappy in its own way.', 'We were somewhere around Barstow on the edge of the desert...', 'It was the best of times, it was the worst of times...']
+    @story_start = rand_story_starts
   end
 
   def show
@@ -34,4 +34,9 @@ class StoriesController < ApplicationController
   def destroy
   end
 
+  def rand_story_starts
+    @story_starts = ['A screaming comes across the sky. /n —Thomas Pynchon, Gravity’s Rainbow', 'It was a bright cold day in April, and the clocks were striking thirteen. /n —George Orwell, 1984', 'It was a pleasure to burn. /n —Ray Bradbury, Fahrenheit 451', 'The Man in Black fled across the desert, and the Gunslinger followed. /n —Stephen King, The Gunslinger', 'The flash projected the outline of the hanged man onto the wall. /n —Arturo Perez-Reverte, The Club Dumas', 'If you’re going to read this, don’t bother. /n —Chuck Palahniuk, Choke', 'Happy families are all alike; every unhappy family is unhappy in its own way. /n —Leo Tolstoy, Anna Karenina', 'We were somewhere around Barstow on the edge of the desert... /n —Hunter S. Thompson, Fear and Loathing in Las Vegas', 'It was the best of times, it was the worst of times... /n -Charles Dickens, A Tale of Two Cities']
+    @rand_story = @story_starts[0 + rand(@story_starts.count)]
+    @split_story = @rand_story.split('/n')
+  end
 end
