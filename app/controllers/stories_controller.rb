eabb2a7dec94abe49418184451280966dd32637e
Garrett Estrin <garrett.estrin@gmail.com>
2016-09-20 18:06:22 -0700

    added changing tag line

--- app/controllers/stories_controller.rb
+++ app/controllers/stories_controller.rb
@@ -1,6 +1,7 @@
 class StoriesController < ApplicationController
   def index
     @stories = Story.all
+    @story_starts = ['A screaming comes across the sky.', 'It was a bright cold day in April, and the clocks were striking thirteen.', 'It was a pleasure to burn.', 'The Man in Black fled across the desert, and the Gunslinger followed.', 'The flash projected the outline of the hanged man onto the wall.', 'If you’re going to read this, don’t bother.', 'Happy families are all alike; every unhappy family is unhappy in its own way.', 'We were somewhere around Barstow on the edge of the desert...', 'It was the best of times, it was the worst of times...']
   end
 
   def show
@@ -32,4 +33,5 @@ class StoriesController < ApplicationController
 
   def destroy
   end
+
 end
