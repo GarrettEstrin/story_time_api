948d6ce134cf56797bdbd99aec0fc78ec9bc076e
Garrett Estrin <garrett.estrin@gmail.com>
2016-09-21 19:18:57 -0700

    added favicon

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -10,7 +10,7 @@ class SessionsController < ApplicationController
       session[:return_to] ||= request.referer
       redirect_to session.delete(:return_to)
     else
-      redirect_to new_session_path
+      redirect_to root_path
     end
   end
 
