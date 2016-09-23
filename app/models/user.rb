195a62c8d71c3df8244078e36006cb5def5f4eba
Garrett Estrin <garrett.estrin@gmail.com>
2016-09-22 20:48:19 -0700

    email confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,7 @@
 class User < ActiveRecord::Base
   has_many :stories, dependent: :destroy
   has_secure_password
+  # Make sure passwords match when registering
+  validates_confirmation_of :password
+  attr_accessor :attribute1, :attribute2
 end
